45383fa14f2758f1c66b271211c40ff2addaecbb
Evan Ristow <evan_ristow@ultimatesoftware.com>
2019-08-05 17:32:08 -0400

    Trying to use PRAW to get reddit posts

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+import praw
 
 app = Flask(__name__)
+reddit = praw.Reddit('DEFAULT', user_agent='React-Flask by /u/nave321')
 
 @app.route('/')
 def index():
@@ -11,13 +13,24 @@ def index():
 def get_subreddit():
     if request.method == 'GET':
         return "It's a GET!"
-    req_data = request.get_json()
+    req_data = request.data.decode("utf-8")
     print('req_data', req_data)
 
-    # subreddit = requests.get('https://www.reddit.com/r/reactjs.json').content
+    # subreddit_data = requests.get(f'https://www.reddit.com/r/{req_data}.json').content
 
-    subreddit = requests.get('https://jsonplaceholder.typicode.com/posts').content
+    subreddit_data = []
+    for submission in reddit.subreddit(req_data).hot(limit=20):
+        subreddit_data.push(submission)
+
+    print('subreddit_data', subreddit_data)
+
+    subreddit = {
+        'data': {
+            'children': subreddit_data
+        }
+    }
 
     print('subreddit', subreddit)
+    print('jsonify subreddit', jsonify(subreddit))
 
-    return subreddit
+    return jsonify(subreddit)
